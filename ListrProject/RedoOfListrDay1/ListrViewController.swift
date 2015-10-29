fb2ab92868ede0b2b4cc6004cb1086e5909bf6ef
HeatherConnery <heatherconnery@gmail.com>
2015-10-29 13:15:11 -0400

    end of part 3

--- ListrProject/RedoOfListrDay1/ListrViewController.swift
+++ ListrProject/RedoOfListrDay1/ListrViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ListrViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // simple data model for stage 2:
+    var myFavoriteGames = ["Super Meat Boy","Doom","Super Mario Bros",
+        "F Zero", "Grand Theft Auto","Donkey Kong Tropical Freeze" ]
+    
+    var arrayOfImages = ["eve.png","Wall-E.png","lunchBox.png","navigator.png","plant.png","cleanUpBot.png"]
 
     
     @IBOutlet weak var listrTableViewOutlet: UITableView!
@@ -27,14 +33,24 @@ class ListrViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return cell
     }
     
-    var myFavoriteGames = ["Super Meat Boy","Doom","Super Mario Bros",
-        "F Zero", "Grand Theft Auto","Donkey Kong Tropical Freeze" ]
+    func  tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // UIAlertController displays an alert message to user and can have associated actions
+//        let alertController = UIAlertController(title: "Row Selected", message: "You've selected a row", preferredStyle: .Alert)
+//        
+//        let okayAction = UIAlertAction(title: "OKAY", style: .Cancel) {
+//            (action) in
+//            println(action)
+//        }
+//        
+//        presentViewController(alertController, animated: true)
+//    }
+//    
     
-    var arrayOfImages = ["eve.png","Wall-E.png","lunchBox.png","navigator.png","plant.png","cleanUpBot.png"]
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -43,15 +59,34 @@ class ListrViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Dispose of any resources that can be recreated.
     }
     
+    
 
-    /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
+        // Get the new view controller using segue.destinationViewController
         // Pass the selected object to the new view controller.
+        if segue.identifier == "presentation"
+        {
+            if let destinationVC = segue.destinationViewController as? ViewController {
+                // Do something cool. Like pass data from one view controller to another.
+                //print("Let's do something cool")
+            }
+        }
     }
-    */
+
+    
+    
+    
 
 }
+
+
+
+
+
+
+
+
+
--- ListrProject/RedoOfListrDay1/ViewController.swift
+++ ListrProject/RedoOfListrDay1/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var companyLogo: UIImageView!
+    
+    @IBOutlet weak var titleHeader: UILabel!
+    
+    @IBOutlet weak var descriptionText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.titleHeader.text = "New Title"
+        self.descriptionText.text = "This is some text showing that we can update this on the fly"
     }
 
     override func didReceiveMemoryWarning() {
